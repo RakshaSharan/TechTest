f0d8a9045f1f940041d165f20084fea16cf0ec7e
Raksha <raksha.kanapuram@sky.uk>
2020-11-26 15:57:05 +0000

    Article test add

---
+++ Features/Steps/steps_def_returnArticles.py
@@ -0,0 +1,67 @@
+from behave import *
+import requests
+
+global_general_variables = {}
+http_request_header = {}
+http_request_body = {}
+http_request_url_query_param = {}
+
+
+@given(u'Set GET api endpoint as "{get_api_endpoint}"')
+def step_impl(context, get_api_endpoint):
+    global_general_variables['GET_api_endpoint'] = get_api_endpoint
+
+
+@when(u'Raise "{http_request_type}" HTTP request')
+def step_impl(context, http_request_type):
+    url_temp = global_general_variables['GET_api_endpoint']
+    if 'GET' == http_request_type:
+        http_request_body.clear()
+        global_general_variables['response_full'] = requests.get(url_temp,
+                                                                 headers=http_request_header,
+                                                                 params=http_request_url_query_param,
+                                                                 data=http_request_body)
+    elif 'POST' == http_request_type:
+        http_request_url_query_param.clear()
+        global_general_variables['response_full'] = requests.post(url_temp,
+                                                                  headers=http_request_header,
+                                                                  params=http_request_url_query_param,
+                                                                  data=http_request_body)
+    elif 'PUT' == http_request_type:
+        http_request_url_query_param.clear()
+        global_general_variables['response_full'] = requests.put(url_temp,
+                                                                 headers=http_request_header,
+                                                                 params=http_request_url_query_param,
+                                                                 data=http_request_body)
+    elif 'DELETE' == http_request_type:
+        http_request_body.clear()
+        global_general_variables['response_full'] = requests.delete(url_temp,
+                                                                    headers=http_request_header,
+                                                                    params=http_request_url_query_param,
+                                                                    data=http_request_body)
+
+
+@then(u'Response http code should be {expected_response_code:d} for "{http_request_type}"')
+def step_impl(context, expected_response_code, http_request_type):
+    global_general_variables['expected_response_code'] = expected_response_code
+    requestType = str(http_request_type)
+    actual_response_code = global_general_variables['response_full'].status_code
+    if requestType == "GET":
+        if actual_response_code == expected_response_code == 200:
+            print(str(global_general_variables['response_full'].json()))
+            assert True
+    elif requestType in ["POST", "PUT", "DELETE"]:
+        print(str(global_general_variables['response_full'].json()))
+        if actual_response_code == expected_response_code == 404:
+            assert True
+    else:
+        print(str(global_general_variables['response_full'].json()))
+        assert False, '***ERROR: Following unexpected error response code received: ' + str(actual_response_code)
+
+
+@then(u'Response HEADER content type should be "{expected_response_content_type}"')
+def step_impl(context, expected_response_content_type):
+    global_general_variables['expected_response_content_type'] = expected_response_content_type
+    actual_response_content_type = global_general_variables['response_full'].headers['Content-Type']
+    if expected_response_content_type not in actual_response_content_type:
+        assert False, '***ERROR: Following unexpected error response content type received: ' + actual_response_content_type
---
+++ Features/returnAllArticles.feature
@@ -0,0 +1,25 @@
+Feature: Returns a list of articles
+
+Background:
+  Given Set GET api endpoint as "https://5f99522350d84900163b8737.mockapi.io/tech-test/articles"
+
+@getTag
+Scenario: Get list of articles
+  When  Raise "GET" HTTP request
+  Then Response http code should be 200 for "GET"
+  And Response HEADER content type should be "application/json"
+
+@postTag
+Scenario: Post list of articles
+  When  Raise "POST" HTTP request
+  Then Response http code should be 404 for "POST"
+
+@putTag
+Scenario: Put list of articles
+  When  Raise "PUT" HTTP request
+  Then Response http code should be 404 for "PUT"
+
+@deleteTag
+Scenario: Delete list of articles
+  When  Raise "DELETE" HTTP request
+  Then Response http code should be 404 for "DELETE"
